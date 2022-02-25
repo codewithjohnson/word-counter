d780e4ef309929c39b8c7aacc8781b1040130771
codewithjohnson <75730906+codewithjohnson@users.noreply.github.com>
2022-02-25 07:53:12 +0100

    ok

--- scripts/script.js
+++ scripts/script.js
@@ -6,40 +6,24 @@ let LetterCounter = document.getElementById('lettercount');
 
 function getWordCount() {
     let TextInputValue = TextInput.value;
+    TextInputValue = TextInputValue.replace(/[^A-Za-z']/g, " ");
     TextInputValue = TextInputValue.trim();
     let Words = TextInputValue.split(' ');
-    
     if (Words[0] === ''){
         WordCounter.textContent = 0;
     }
     else{
         WordCounter.textContent = Words.length;
-        Chars = Words.join('');
-        LetterCounter.textContent = Chars.length;
+        let Chars = Words.join('');
+        console.log(Chars);
+        if (Chars[0] === ''){
+            LetterCounter.textContent = 0;
+        }
+        else{
+            LetterCounter.textContent = Chars.length;
+        }
+        
         
     }
 }
 
-// const str = 'i am here';
-// let l = Array.from(str.split(' '));
-// console.log(l);
-// l = l.join('');
-// console.log(l);
-// console.log(l.length);
-// console.log();
-
-// const j = str.split('');
-// console.log(j);
-// console.log();
-
-// const a = Array.from(str.split(' '));
-// console.log(a);
-// console.log();
-
-// const b = Array.from(str);
-// console.log(b);
-// console.log();
-
-// const p = b.join();
-// console.log(p);
-// console.log();
